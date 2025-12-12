517f46421116286c15122dcb446ce7769affffb8
AnitaTec <anitateclava@gmail.com>
2025-12-12 21:42:12 +0200

    fix tokens lifetime to 15m and 7d

--- src/utils/creteTokens.ts
+++ src/utils/creteTokens.ts
@@ -1,10 +1,20 @@
+import jwt from "jsonwebtoken";
 import { Types } from "mongoose";
 
-import { generateToken } from "./jwt.js";
+const { JWT_SECRET } = process.env;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET not define in environment variables");
+}
 
 const creteTokens = (id: Types.ObjectId) => {
-  const accessToken: string = generateToken({ id }, { expiresIn: "15m" });
-  const refreshToken: string = generateToken({ id }, { expiresIn: "7d" });
+  const payload = { id };
+
+  // accessToken живет 15 минут
+  const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" });
+
+  // refreshToken живет 7 дней
+  const refreshToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
 
   return { accessToken, refreshToken };
 };
